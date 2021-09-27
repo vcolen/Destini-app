ed93bda1c7fa727f68eb2c89f5c129f77c55ea05
VIctor <colen.victor@icloud.com>
2021-09-27 13:11:58 -0300

    add app

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice1NextStory: Int
+    var choice2: String
+    var choice2NextStory: Int
+    
+    init(title: String, choice1: String, choice2: String, choice1NextStory: Int, choice2NextStory: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choice1NextStory = choice1NextStory
+        self.choice2NextStory = choice2NextStory
+    }
+    
+   
+    
+}
